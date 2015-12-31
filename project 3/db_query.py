10c3ba9298f3c7ee59043edd4fd23ccec1f730c4
kshannon <k.shannon.ucsd@gmail.com>
2015-12-30 21:54:37 -0800

    updated query file for project 3

--- project 3/db_query.py
+++ project 3/db_query.py
@@ -1,10 +1,42 @@
 import pprint
 from pymongo import MongoClient
 
-client = MongoClient()
-db = client.seattle_osm_database
+
+def make_pipeline():
+	# complete the aggregation pipeline
+	pipeline = [ {"$find": {"amenity": "bike"}}]
+	return pipeline
 
 
-print "size of collection: ", db.way_node_collection.count()
+def aggregate(db, pipeline):
+	result = db.aggregate(pipeline)
+	#pprint.pprint(result)
+	return result
 
-pprint.pprint(db.way_node_collection.find_one())
+
+if __name__ == "__main__":
+	# Initial local db setup
+	client = MongoClient()
+	db = client.seattle_osm_database.way_node_collection
+
+	pipeline = make_pipeline()
+	result = aggregate(db, pipeline)
+
+	for document in result:
+		pprint.pprint(document)
+
+
+#print "size of collection: ", db.way_node_collection.count()
+
+#pprint.pprint(db.way_node_collection.find_one())
+
+# time min max
+# Bike ammentiies
+# relegion/church
+# road types
+# num of sources 
+# look at shops/amenities. 
+
+# Query for Min/Max 
+# get all timestamps into a  list then sort them and 
+# retrieve first and last entry
