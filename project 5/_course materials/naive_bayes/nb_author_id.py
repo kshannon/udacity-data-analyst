6b3278321d04da0b848d792de378cdb2a68d8e18
kshannon <k.shannon.ucsd@gmail.com>
2016-01-05 19:35:35 -0800

    finished lesson 2 sum

--- project 5/_course materials/naive_bayes/nb_author_id.py
+++ project 5/_course materials/naive_bayes/nb_author_id.py
@@ -14,6 +14,7 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
 
 
 ### features_train and features_test are the features for the training
@@ -22,12 +23,31 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
 ### your code goes here ###
+print type(preprocess())
+print type(features_train)
+print len(features_train)
+
+
+clf = GaussianNB()
+
+# counting time for fitting model 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+# training time = 0.921 s
+
+# counting time for prediction
+t0 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t0, 3), "s"
+# predicting time = 0.185 s
+
+accuracy = clf.score(features_test, labels_test)
+print accuracy
+# score = .9738
 
 
 #########################################################
-
 
--- project 5/_course materials/svm/svm_author_id.py
+++ project 5/_course materials/svm/svm_author_id.py
@@ -20,11 +20,47 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
+# these allow us to train the data using only 1% of data.
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100] 
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+#clf = svm.SVC(kernel='linear')
+clf = svm.SVC(kernel='rbf', C=10000)
+
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+
+t0 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t0, 3), "s"
+
+
+print "C at 10000.0: accuracy_score = ", accuracy_score(labels_test, pred)
+
+# getting predictions for specific indexes
+answer_10 = pred[10]
+answer_26 = pred[26]
+answer_50 = pred[50]
+print "answer for 10: %s, for 26: %s, for 50: %s" % (answer_10, answer_26, answer_50)
+
+# getting classified count for predicitions
+chris = 0
+sara = 0
+for prediction in pred:
+	if prediction == 1:
+		chris += 1
+	sara += 1
+print "number of predictions for chris: %s, sara: %s and total: %s" % (chris, sara, len(pred))
 
 #########################################################
 
-
+# play hardware bell sound
+print('\a')
