d9b400c3ae58fc718d39d9b9b0587e34d419a2be
kshannon <k.shannon.ucsd@gmail.com>
2016-01-09 00:38:47 -0800

    project 5: lesson 5 code
    2/3 way done

--- project 5/_course materials/datasets_questions/explore_enron_data.py
+++ project 5/_course materials/datasets_questions/explore_enron_data.py
@@ -19,4 +19,41 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+print "length of Enron Dict: ", len(enron_data)
+
+# printing a sample of data and number of total samples.
+count = True
+for k, v in enron_data.iteritems():
+	if count:
+		print "Sample {Key : Value} pair:" 
+		print "key: %s | value: %s" % (k, v)
+		print "Number of features: ", len(v)
+	count = False
+
+# counting number of people_of_interest using the 'poi' feature.
+# This comes from the email addresses file... poi_email_address.py
+num_of_poi = 0
+for k, dictionary in enron_data.iteritems():
+	for k, v in dictionary.iteritems():
+		if k == 'poi': 
+			if v: num_of_poi += 1
+print 'Number of "poi": ', num_of_poi
+
+
+# accessing James Prentice's stock value
+print "Prentice's total stock value: ", (enron_data["PRENTICE JAMES"]
+										["total_stock_value"])
+
+
+# accessing Wesley Colwell num of email messages
+print "Colwell's from messages to poi: ", (enron_data["COLWELL WESLEY"]
+											["from_this_person_to_poi"])
+
+# accessing Jefferey Skilling's stock options exercised
+print "Skillings's total stock options exercised: ",( 
+	enron_data["SKILLING JEFFREY K"]['exercised_stock_options'])
+
+# hardware bell
+print ('\a')
+
 
