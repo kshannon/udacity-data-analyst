c10f2329fa32e9a859426eb63499ce17905effff
kshannon <k.shannon.ucsd@gmail.com>
2016-01-19 02:15:10 -0800

    updated project 5 final project

--- project 5/_course materials/final_project/poi_id.py
+++ project 5/_course materials/final_project/poi_id.py
@@ -4,15 +4,37 @@ import sys  #sys.exit()
 import pickle
 sys.path.append("../tools/")
 import matplotlib.pyplot as plt
+import numpy as np
 
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
-from sklearn.preprocessing import Imputer
+from sklearn import preprocessing
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+### Adding in all features now, will trim later on with PCA etc.
+### 143 * 18 = 2574 data points....
+features_list = ["poi",
+				"salary",
+				"to_messages",
+				"deferral_payments",
+				"total_payments",
+				"exercised_stock_options",
+				"bonus",
+				"restricted_stock",
+				"shared_receipt_with_poi",
+				"restricted_stock_deferred",
+				"total_stock_value",
+				"expenses",
+				"loan_advances",
+				"from_messages",
+				#"other",
+				"from_this_person_to_poi",
+				"director_fees",
+				"deferred_income",
+				"long_term_incentive",
+				"from_poi_to_this_person"]
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
@@ -23,42 +45,48 @@ with open("final_project_dataset.pkl", "r") as data_file:
 for k,v in data_dict.iteritems():
 	if k == "THE TRAVEL AGENCY IN THE PARK": continue #print v['poi']
 	if k == "TOTAL": continue #print v
-# remove TOTAL and THE TRAVEL AGENCY IN THE PARK data as an outlier. 
+	if k == 'LOCKHART EUGENE E': continue #print v
+
+### remove TOTAL and THE TRAVEL AGENCY IN THE PARK data as outliers.
+### EUGENE had no data, all are NaN except for {'poi':'False'}. Remove him as well.
 del data_dict['TOTAL']
 del data_dict['THE TRAVEL AGENCY IN THE PARK']
+del data_dict['LOCKHART EUGENE E']
 
-# returning simple info about data set: NaNs features and data points
+### returning simple info about data set: NaNs features and data points
 num_NaNs = 0
 num_data_points = 0
 for k,v in data_dict.iteritems():
-	if k == 'LOCKHART EUGENE E': print v
 	for key, value in v.iteritems():
+		#if key == 'poi' and value == True: print "AHHHHH YEAHHHHH"
 		num_data_points += 1
 		num_features = len(v)
 		if value == 'NaN':
 			num_NaNs += 1
+
 
 print ("Number of People under Investigation: %s, \
 		Number of Data Points: %s, Number of Features: %s") % (
 			len(data_dict), num_data_points, num_features)
 print "Percentage of data points as NaNs: %s" % (num_NaNs/float(num_data_points))
 
-# Data Imputation using Mediun vs Mean or linear regression for NaN values
-# Use SkLearn Data imputate: 
-# http://scikit-learn.org/stable/modules/preprocessing.html#imputation-of-missing-values
-
-
-
-
-
 
 ### Task 3: Create new feature(s)
+
+# to do.....
+
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
 
 ### Extract features and labels from dataset for local testing
-data = featureFormat(my_dataset, features_list, sort_keys = True)
+data = featureFormat(my_dataset, features_list, sort_keys = True, remove_NaN = True)
 labels, features = targetFeatureSplit(data)
+
+print np.shape(features)
+print features[100]
+print labels
+
+
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
@@ -68,7 +96,13 @@ labels, features = targetFeatureSplit(data)
 
 # Provided to give you a starting point. Try a variety of classifiers.
 from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
+clf2 = GaussianNB()
+
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier(random_state=0)
+
+
+
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
@@ -82,13 +116,15 @@ from sklearn.cross_validation import train_test_split
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
 
+
+
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
 ### that the version of poi_id.py that you submit can be run on its own and
 ### generates the necessary .pkl files for validating your results.
 
 # Commented out pickle pickle to make running easier
-#dump_classifier_and_data(clf, my_dataset, features_list)
+dump_classifier_and_data(clf, my_dataset, features_list)
 
 # Bell Sound When Done
 print ('\a')
