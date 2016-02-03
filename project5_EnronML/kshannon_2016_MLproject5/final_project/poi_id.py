13f5e73673b4a7afa3886aff0148b72ca9b26918
kshannon <k.shannon.ucsd@gmail.com>
2016-02-03 00:06:42 -0800

    updates to ML poi_id.py code and AB testing

--- project5_EnronML/kshannon_2016_MLproject5/final_project/poi_id.py
+++ project5_EnronML/kshannon_2016_MLproject5/final_project/poi_id.py
@@ -17,6 +17,7 @@ from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.feature_selection import SelectKBest, f_classif
 from sklearn.decomposition import PCA
 from sklearn.grid_search import GridSearchCV
+from sklearn.cross_validation import StratifiedShuffleSplit
 from sklearn.pipeline import Pipeline
 
 from sklearn.naive_bayes import GaussianNB
@@ -87,7 +88,13 @@ my_dataset = data_dict
 data = featureFormat(my_dataset, features_list, sort_keys = True, remove_NaN = True)
 labels, features = targetFeatureSplit(data)
 
+### X or features is a list of lists containing the data
+### y or labels are the classifier labels for POI or non POI
+X,y = features, labels
+X_new = SelectKBest().fit_transform(X, y)
+print X_new.scores_
 
+sys.exit()
 
 ##################### Task 4: Try a varity of classifiers #####################
 
@@ -180,7 +187,11 @@ param_dict = {}
 # 			  #'dt__min_samples_split' : [2, 4, 8],
 # 			  #'dt__min_samples_leaf' : []}
 
-clf = GridSearchCV(decision_tree_pipeline, param_dict)
+#clf = GridSearchCV(decision_tree_pipeline, param_dict)
+
+folds = 1000
+cv = StratifiedShuffleSplit(labels, folds, random_state = 42)
+clf = GridSearchCV(decision_tree_pipeline, param_dict, cv=cv)
 
 
 ################### -- Decision Tree [end] -- ###################
@@ -191,6 +202,90 @@ clf = GridSearchCV(decision_tree_pipeline, param_dict)
 
 dump_classifier_and_data(clf, my_dataset, features_list)
 
+
+
+
+#####################################################################
+#####################################################################
+##################### Tester Code to Build From #####################
+#####################################################################
+#####################################################################
+
+
+
+import pickle
+import sys
+from sklearn.cross_validation import StratifiedShuffleSplit
+sys.path.append("../tools/")
+from feature_format import featureFormat, targetFeatureSplit
+
+PERF_FORMAT_STRING = "\
+\tAccuracy: {:>0.{display_precision}f}\tPrecision: {:>0.{display_precision}f}\t\
+Recall: {:>0.{display_precision}f}\tF1: {:>0.{display_precision}f}\tF2: {:>0.{display_precision}f}"
+RESULTS_FORMAT_STRING = "\tTotal predictions: {:4d}\tTrue positives: {:4d}\tFalse positives: {:4d}\
+\tFalse negatives: {:4d}\tTrue negatives: {:4d}"
+
+def test_classifier(clf, dataset, feature_list, folds = 1000):
+    print feature_list # I added this line to print features list to ml_results.txt
+    data = featureFormat(dataset, feature_list, sort_keys = True)
+    labels, features = targetFeatureSplit(data)
+    cv = StratifiedShuffleSplit(labels, folds, random_state = 42)
+    true_negatives = 0
+    false_negatives = 0
+    true_positives = 0
+    false_positives = 0
+    for train_idx, test_idx in cv: 
+        features_train = []
+        features_test  = []
+        labels_train   = []
+        labels_test    = []
+        for ii in train_idx:
+            features_train.append( features[ii] )
+            labels_train.append( labels[ii] )
+        for jj in test_idx:
+            features_test.append( features[jj] )
+            labels_test.append( labels[jj] )
+        
+        ### fit the classifier using training set, and test on test set
+        clf.fit(features_train, labels_train)
+
+        # print "BEGIN: SelectKBest Info..."
+        # print select.clf.scores_
+        # print clf.pvalues_
+        # print clf.get_support
+        # print clf.get_params
+        # print "END: SelectKBest Info..."
+
+
+        predictions = clf.predict(features_test)
+        for prediction, truth in zip(predictions, labels_test):
+            if prediction == 0 and truth == 0:
+                true_negatives += 1
+            elif prediction == 0 and truth == 1:
+                false_negatives += 1
+            elif prediction == 1 and truth == 0:
+                false_positives += 1
+            elif prediction == 1 and truth == 1:
+                true_positives += 1
+            else:
+                print "Warning: Found a predicted label not == 0 or 1."
+                print "All predictions should take value 0 or 1."
+                print "Evaluating performance for processed predictions:"
+                break
+    try:
+        total_predictions = true_negatives + false_negatives + false_positives + true_positives
+        accuracy = 1.0*(true_positives + true_negatives)/total_predictions
+        precision = 1.0*true_positives/(true_positives+false_positives)
+        recall = 1.0*true_positives/(true_positives+false_negatives)
+        f1 = 2.0 * true_positives/(2*true_positives + false_positives+false_negatives)
+        f2 = (1+2.0*2.0) * precision*recall/(4*precision + recall)
+        print clf
+        print PERF_FORMAT_STRING.format(accuracy, precision, recall, f1, f2, display_precision = 5)
+        print RESULTS_FORMAT_STRING.format(total_predictions, true_positives, false_positives, false_negatives, true_negatives)
+        print ""
+    except:
+        print "Got a divide by zero when trying out:", clf
+        print "Precision or recall may be undefined due to a lack of true positive predicitons."
 
 
 
