770730cc9e2db7a0a096739b4c7da519dad3c81d
Kyle Shannon <k.shannon.ucsd@gmail.com>
2016-08-05 23:58:42 -0700

    adding readme updates and typing up feedback from people for first version

--- Project6_DataViz/README.md
+++ Project6_DataViz/README.md
@@ -2,11 +2,42 @@ a README.md file that includes four sections...
 
 Summary - in no more than 4 sentences, briefly introduce your data visualization and add any context that can help readers understand it
 
-My visualization looks at a Major League Baseball data set and identifies some trends that have to do with handedness, e.g. left vs right hand. It is important to note that the MLB has a higher number of lefties and ambidextrious batters than the general population. 
+My visualization looks at a Major League Baseball data set and identifies some trends that have to do with handedness, e.g. left vs right hand. It is important to note that the MLB has a higher number of lefties and ambidextrous batters than the general population. There is a significant amount of players with zero home runs and a zero (0.000) batting average, these are most likely American League Pitchers. The visuals and data summary seem to suggest that there is def a difference in performance between lefties and righties in the MLB, this follows conventional wisdom and matches up with what other sources have reported on the issue.
 
 Design - explain any design choices you made including changes to the visualization after collecting feedback
 
+I wanted to implement a bar chart and scatter plot in my visual. I set out to use a bar chart, though probably verbose, to show the number of players grouped by handedness. A scatter plot was a perfect opportunity to look at relationships between Home runs and batting average. An interesting find was that almost no lefties dropped below the .200 batting average. Some feedback I received suggested that I make the scatterplot more interactive and either include an alpha for the individual points, or allow the user to hide data based on handedness. I decided to include an interaction that allowed a user to hide data. This made it easier to reveal the true scatter plot shape for each handedness group.
+
+Another change I made was to add tables with summary statistics to show how each handedness compared to each other with respect to one dimension of data. This was good, because the scatterplot does not do the best job showing that lefties were more likely to have more home runs and better batting averages. This was a good suggestion.
+
 Feedback - include all feedback you received from others on your visualization from the first sketch to the final visualization
+
+Feedback 1:
+
+[Q: What do you notice in the visualization?] [A:] 
+[Q: What questions do you have about the data?] [A:]
+[Q: What relationships do you notice?] [A:]
+[Q: What do you think is the main takeaway from this visualization?] [A:]
+[Q: Is there something you don’t understand in the graphic?] [A:]
+
+
+
+Feedback 2:
+
+[Q: What do you notice in the visualization?] [A:] 
+[Q: What questions do you have about the data?] [A:]
+[Q: What relationships do you notice?] [A:]
+[Q: What do you think is the main takeaway from this visualization?] [A:]
+[Q: Is there something you don’t understand in the graphic?] [A:]
+
+Feedback 3:
+
+[Q: What do you notice in the visualization?] [A:] 
+[Q: What questions do you have about the data?] [A:]
+[Q: What relationships do you notice?] [A:]
+[Q: What do you think is the main takeaway from this visualization?] [A:]
+[Q: Is there something you don’t understand in the graphic?] [A:]
+
 
 Resources - list any sources you consulted to create your visualization
 
@@ -16,4 +47,4 @@ References are listed below:
 2. Annett, Marian (2002). Handedness and Brain Asymmetry. Psychology Press.
 3. http://www.newsweek.com/science-why-lefties-make-better-baseball-players-92783
 4. http://blogs.discovermagazine.com/discoblog/2008/07/08/why-do-so-many-lefties-play-baseball-its-built-for-them/
-5.
+5. http://dimplejs.org/advanced_examples_viewer.html?id=advanced_interactive_legends
---
+++ Project6_DataViz/Version 1 of visual/js/main.js
@@ -0,0 +1,120 @@
+    var width = 1000,
+        height = 600;
+    
+// Chart1 - box plot count for Baseball Data Set
+
+    var svg1 = dimple.newSvg("#chart1", width, height);
+    d3.csv("baseball_data_v2.csv", function(data){
+        var chart = new dimple.chart(svg1, data);
+
+        var x = chart.addCategoryAxis("x", "handedness");
+        x.title = "Batting Stance";
+        x.fontSize = "12px";
+        
+        var y = chart.addMeasureAxis("y", "count");
+        y.title = "MLB Player Count";
+        y.fontSize = "12px";
+        
+        chart.addSeries(["handedness"], dimple.plot.bar); // updates tool tip too!
+        chart.addLegend(850, 120, 40, 400, "right")
+        //chart.assignColor("handedness", [], stroke, .25) //this break my first chart.. worked before.
+
+        svg1.append("text")
+            .attr("x", chart._xPixels() + chart._widthPixels() / 2)
+            .attr("y", chart._yPixels() - 5)
+            .style("text-anchor", "middle")
+            .style("font-weight", "bold")
+            .style("font-size", "20px")
+            .text("Batting Stances of MLB Players");
+
+    chart.draw();
+    }); 
+
+// Chart2 - scatter plot count for Baseball Data Set
+
+    var svg2 = dimple.newSvg("#chart2", width, height);
+    d3.csv("baseball_data_v2.csv", function(data){
+            var chart2 = new dimple.chart(svg2, data);
+
+            var x2 = chart2.addMeasureAxis("x", "HR");
+            x2.title = "Number of Home Runs";
+            x2.fontSize = "12px";
+
+            
+            var y2 =chart2.addMeasureAxis("y", "avg");
+            y2.title = "Batting Average";
+            y2.fontSize = "12px";
+            y2.ticks = 10;
+            y2.tickFormat = ',.3f';
+
+            //chart2.addMeasureAxis("z", "Operating Profit");
+            chart2.addSeries(["name", "handedness"], dimple.plot.bubble);
+            //chart2.addLegend(850, 120, 40, 400, "right")
+            var Legend2 = chart2.addLegend(850, 480, 40, 400, "right");
+
+
+        svg2.append("text")
+            .attr("x", chart2._xPixels() + chart2._widthPixels() / 2)
+            .attr("y", chart2._yPixels() - 5)
+            .style("text-anchor", "middle")
+            .style("font-weight", "bold")
+            .style("font-size", "20px")
+            .text("Batting Average and Home Run Trend by Handedness");
+            
+            chart2.draw();
+
+        // Doing this we orphan the legend. This
+        // means it will not respond to graph updates.  Without this the legend
+        // will redraw when the chart refreshes removing the unchecked item and
+        // also dropping the events we define below.
+            chart2.legends = [];
+
+        // This block simply adds the legend title. 
+            svg2.selectAll("title_text")
+              .data(["Click legend to","show/hide stances:"])
+              .enter()
+              .append("text")
+                .attr("x", 800)
+                .attr("y",  function (d, i) { return 470 + i * 14; })
+                .style("font-family", "sans-serif")
+                .style("font-size", "10px")
+                .style("color", "Black")
+                .text(function (d) { return d; });
+
+            // Get a unique list of Owner values to use when filtering
+            var filterValues = dimple.getUniqueValues(data, "handedness");
+
+            // Get all the rectangles from our now orphaned legend
+            Legend2.shapes.selectAll("rect")
+
+        
+              // Add a click event to each rectangle
+              .on("click", function (e) {
+                // This indicates whether the item is already visible or not
+                var hide = false;
+                var newFilters = [];
+                // If the filters contain the clicked shape hide it
+                filterValues.forEach(function (f) {
+                  if (f === e.aggField.slice(-1)[0]) {
+                    hide = true;
+                  } else {
+                    newFilters.push(f);
+                  }
+                });
+                // Hide the shape or show it
+                if (hide) {
+                  d3.select(this).style("opacity", 0.2);
+                } else {
+                  newFilters.push(e.aggField.slice(-1)[0]);
+                  d3.select(this).style("opacity", 0.8);
+                }
+                // Update the filters
+                filterValues = newFilters;
+                // Filter the data
+                chart2.data = dimple.filterData(data, "handedness", filterValues);
+                // Passing a duration parameter makes the chart animate. Without
+                // it there is no transition
+                chart2.draw(); 
+            });
+        });
+
